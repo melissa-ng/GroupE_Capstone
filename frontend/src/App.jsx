b72378901f6449444b69168bd77cdfa4c5e84c2a
melissa-ng <melissa.j.ng-1@ou.edu>
2025-04-09 12:37:23 -0500

    sign up page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import "./App.css";
 import Sidebar from "./components/SideBar/SideBar";
 import SignIn from "./pages/SignInPage/SignInPage";
 import DispatchersPage from "./pages/DispatchersPage/DispatchersPage";
+import SignUp from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
               <Route path="/dispatchers" element={<DispatchersPage />} />
               <Route path="/" element={<TranscriptUpload />} />
               <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
             </Routes>
           </div>
         </div>
